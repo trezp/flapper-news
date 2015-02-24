edf8bd9f72320adc5829b9afa01757ec29bdf37d
Treasure Porth <treasure.rose@gmail.com>
2015-02-24 12:51:13 -0800

    Added commenting system.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flapper-news
+
+A Reddit clone built with Angular, Express, node.js and MongoDB, with help from the thinkster.io MEAN stack tutorial. 
--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-var app = angular.module('flapperNews', []); 
+var app = angular.module('flapperNews', ['ui.router']); 
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl',
+			});
+		$stateProvider
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			
+			});
+			
+			
+		$urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o; 
+}]);
 
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
 	$scope.test = "Hello world!";
-	$scope.posts = [
-	 {title: 'post 1', upvotes: 5},
-	 {title: 'post 2', upvotes: 2},
-	 {title: 'post 3', upvotes: 15},
-	 {title: 'post 4', upvotes: 56},
-	 {title: 'post 5', upvotes: 34},
-	]
+	$scope.posts = posts.posts;
 	$scope.addPost = function(){
 		if(!$scope.title || $scope.title === ''){
 			return;
@@ -18,7 +43,11 @@ function($scope){
 		$scope.posts.push({
 			title: $scope.title,
 			link: $scope.link,
-			upvotes: 0
+			upvotes: 0,
+			comments: [
+				{author: 'Joe', body: 'AWESOME POST', upvotes: 0}, 
+				{author: 'Bob', body: "YOU SUCK", upvotes: 0}
+			]
 		});
 		$scope.title = '';
 		$scope.link = '';
@@ -29,3 +58,19 @@ function($scope){
 } 	
 	]);
 
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts', 
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+		$scope.post.comments.push({
+			body: $scope.body, 
+			author: 'user', 
+			upvotes: 0
+		});
+		$scope.body = '';
+		}
+}]);
